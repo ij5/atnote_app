d2cb9f2e3ce9df646cd210ce56da9c7bdf9e31af
이재희 <jhlee838@gmail.com>
2021-01-26 20:01:16 +0900

    update

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,10 +1,17 @@
 import 'package:atnote/home.dart';
 import 'package:atnote/index.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hive/hive.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class Settings extends StatelessWidget{
+class Settings extends StatefulWidget{
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
   var poem;
 
   initPoem()async{
@@ -12,9 +19,33 @@ class Settings extends StatelessWidget{
     return poem;
   }
 
+  var authPref = false;
+
+  Future setAuthPrefs(auth)async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if(prefs.getBool('auth')==null){
+      prefs.setBool('auth', auth);
+    }
+    prefs.setInt('auth', auth);
+  }
+  Future getAuthPrefs()async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      authPref = prefs.getBool('auth');
+    });
+    return authPref;
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    getAuthPrefs();
+  }
+
   @override
   Widget build(BuildContext context) {
     initPoem();
+
     return WillPopScope(
       onWillPop: (){
         Get.off(Home());
@@ -31,19 +62,42 @@ class Settings extends StatelessWidget{
           ),
         ),
         body: Container(
-          child: Column(
-            children: [
-              FlatButton(
-                child: Text("RESET"),
-                onPressed: (){
-                  poem.delete('file');
-                },
+          child: SizedBox.expand(
+            child: Center(
+              child: Column(
+                children: [
+                  Switch(
+                    value: authPref,
+                    activeColor: Colors.black,
+                    inactiveThumbColor: Colors.grey,
+                    inactiveTrackColor: Colors.grey,
+                    onChanged: (value){
+                      getAuthPrefs();
+                      if(authPref){
+                        setAuthPrefs(0);
+                      }else{
+                        setAuthPrefs(1);
+                      }
+                    },
+                  ),
+                  FlatButton(
+                    child: Text("RESET"),
+                    onPressed: (){
+                      poem.delete('file');
+                    },
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      side: BorderSide(width: 1, style: BorderStyle.solid, color: Colors.red),
+                    ),
+                    color: Colors.white,
+                    textColor: Colors.red,
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
     );
   }
-
 }
