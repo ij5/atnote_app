1ac35607328606231672c26970bec4748a488aed
이재희 <jhlee838@gmail.com>
2021-01-29 23:04:01 +0900

    Update search.dart

--- lib/search.dart
+++ lib/search.dart
@@ -57,97 +57,95 @@ class _SearchState extends State<Search> {
                   child: CircularProgressIndicator(),
                 );
               }
-              return SingleChildScrollView(
-                child: ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-                  itemCount: snapshot.data.get('file')==null?0:snapshot.data.get('file').length,
-                  itemBuilder: (BuildContext context, int i){
-                    var file = File(snapshot.data.get('file')[i]);
-                    var rawContent = file.readAsStringSync();
-                    var content = jsonDecode(rawContent);
-                    if(content[0]['trash']=="true"){
-                      return SizedBox.shrink();
-                    }
-                    if(!rawContent.toLowerCase().contains(_query.toLowerCase())){
-                      return SizedBox.shrink();
-                    }
-                    return Dismissible(
-                      key: UniqueKey(),
-                      onDismissed: (direction){
-                        content[0]['trash'] = "true";
-                        file.writeAsString(jsonEncode(content));
-                        Scaffold.of(context)..removeCurrentSnackBar()..showSnackBar(SnackBar(content: Text("Moved to trash.")));
-                      },
-                      child: GestureDetector(
-                        onTap: (){
-                          Get.to(View(), arguments: [jsonEncode(content), file, null]).then((value){
-                            setState(() {
-                              content = value[0];
-                            });
+              return ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+                itemCount: snapshot.data.get('file')==null?0:snapshot.data.get('file').length,
+                itemBuilder: (BuildContext context, int i){
+                  var file = File(snapshot.data.get('file')[i]);
+                  var rawContent = file.readAsStringSync();
+                  var content = jsonDecode(rawContent);
+                  if(content[0]['trash']=="true"){
+                    return SizedBox.shrink();
+                  }
+                  if(!rawContent.toLowerCase().contains(_query.toLowerCase())){
+                    return SizedBox.shrink();
+                  }
+                  return Dismissible(
+                    key: UniqueKey(),
+                    onDismissed: (direction){
+                      content[0]['trash'] = "true";
+                      file.writeAsString(jsonEncode(content));
+                      Scaffold.of(context)..removeCurrentSnackBar()..showSnackBar(SnackBar(content: Text("Moved to trash.")));
+                    },
+                    child: GestureDetector(
+                      onTap: (){
+                        Get.to(View(), arguments: [jsonEncode(content), file, null]).then((value){
+                          setState(() {
+                            content = value[0];
                           });
-                        },
-                        child: Stack(
-                          children: [
-                            Container(
-                              margin: EdgeInsets.fromLTRB(20, 15, 20, 15),
-                              padding: EdgeInsets.all(30),
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                border: Border.all(color: Color(0xffd6d6d6), width: 0),
-                                boxShadow: [BoxShadow(
-                                    color: Colors.black12,
-                                    offset: Offset(7, 7),
-                                    blurRadius: 10,
-                                    spreadRadius: 0
-                                ),],
-                                borderRadius: BorderRadius.all(Radius.circular(15)),
-                              ),
-                              child: Center(
-                                child: Text(content[0]['title']),
-                              ),
+                        });
+                      },
+                      child: Stack(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.fromLTRB(20, 15, 20, 15),
+                            padding: EdgeInsets.all(30),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border.all(color: Color(0xffd6d6d6), width: 0),
+                              boxShadow: [BoxShadow(
+                                  color: Colors.black12,
+                                  offset: Offset(7, 7),
+                                  blurRadius: 10,
+                                  spreadRadius: 0
+                              ),],
+                              borderRadius: BorderRadius.all(Radius.circular(15)),
                             ),
-                            Positioned(
-                              child: content[0]['heart']=="true"
-                                  ?InkWell(
-                                child: Icon(Icons.favorite, color: Colors.red,),
-                                onTap: (){
-                                  setState(() {
-                                    content[0]['heart'] = "false";
-                                  });
-                                  file.writeAsStringSync(jsonEncode(content));
-                                },
-                              )
-                                  :InkWell(
-                                child: Icon(Icons.favorite_outline),
-                                onTap: (){
-                                  setState(() {
-                                    content[0]['heart'] = "true";
-                                  });
-                                  file.writeAsStringSync(jsonEncode(content));
-                                },
-                              ),
-                              right: 30,
-                              bottom: 25,
+                            child: Center(
+                              child: Text(content[0]['title']),
                             ),
-                          ],
-                        ),
+                          ),
+                          Positioned(
+                            child: content[0]['heart']=="true"
+                                ?InkWell(
+                              child: Icon(Icons.favorite, color: Colors.red,),
+                              onTap: (){
+                                setState(() {
+                                  content[0]['heart'] = "false";
+                                });
+                                file.writeAsStringSync(jsonEncode(content));
+                              },
+                            )
+                                :InkWell(
+                              child: Icon(Icons.favorite_outline),
+                              onTap: (){
+                                setState(() {
+                                  content[0]['heart'] = "true";
+                                });
+                                file.writeAsStringSync(jsonEncode(content));
+                              },
+                            ),
+                            right: 30,
+                            bottom: 25,
+                          ),
+                        ],
                       ),
-                      background: Container(
-                        margin: EdgeInsets.fromLTRB(20, 15, 20, 15),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(bottomRight: Radius.circular(15), topRight: Radius.circular(15)),
-                          color: Colors.red,
-                        ),
-                        alignment: Alignment.centerRight,
-                        padding: EdgeInsets.all(20),
-                        child: Icon(Icons.delete, color: Colors.white,),
+                    ),
+                    background: Container(
+                      margin: EdgeInsets.fromLTRB(20, 15, 20, 15),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(bottomRight: Radius.circular(15), topRight: Radius.circular(15)),
+                        color: Colors.red,
                       ),
-                      direction: DismissDirection.endToStart,
-                    );
-                  },
-                ),
+                      alignment: Alignment.centerRight,
+                      padding: EdgeInsets.all(20),
+                      child: Icon(Icons.delete, color: Colors.white,),
+                    ),
+                    direction: DismissDirection.endToStart,
+                  );
+                },
               );
             },
           );
